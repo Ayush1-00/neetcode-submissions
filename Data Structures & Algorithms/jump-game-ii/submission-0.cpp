e2df5a44b68e262fa96cfc49014a57ede477c4a0
Ayush Khandelwal <121782836+Ayush1-00@users.noreply.github.com>
2026-08-10 08:33:16 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int jumps=0;
+        int far=0;
+        int ce=0;
+        for(int i=0;i<n-1;i++){
+            far=max(far,i+nums[i]);
+            if(i==ce){
+                jumps++;
+                ce=far;
+            }
+        }
+        return jumps;
+    }
+};
